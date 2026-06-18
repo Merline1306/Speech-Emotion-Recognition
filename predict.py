8cb744fedb8b90a45a866284ad5ddf1263db8780
Merline Princy J <merlineprincyy@gmail.com>
2026-06-18 21:16:43 +0530

    Add files via upload

--- predict.py
+++ predict.py
@@ -1,14 +1,21 @@
+import os
 import json
 import numpy as np
 
 from tensorflow.keras.models import load_model
-
 from feature_extraction import extract_features
+
+print("Current Directory:", os.getcwd())
+print("Files in Current Directory:", os.listdir())
 
 model = load_model("emotion_model.keras")
 
-with open("emotion_labels.json","r") as f:
-    labels = json.load(f)
+with open("emotion_labels.json", "r", encoding="utf-8") as f:
+    content = f.read()
+
+print("JSON Content:", repr(content))
+
+labels = json.loads(content)-[]
 
 
 def predict(audio_path):
