6d838ce056029c7f63f2d0e9a24077328ea40f86
Merline Princy J <merlineprincyy@gmail.com>
2026-06-20 11:37:31 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -1,36 +1,25 @@
-import os
 import json
 import numpy as np
-
 from tensorflow.keras.models import load_model
 from feature_extraction import extract_features
 
-print("Current Directory:", os.getcwd())
-print("Files in Current Directory:", os.listdir())
-
+# Load trained model
 model = load_model("emotion_model.keras")
 
-with open("emotion_labels.json", "r", encoding="utf-8") as f:
-    content = f.read()
+# Load emotion labels
+with open("emotion_labels.json", "r") as f:
+    labels = json.load(f)
 
-print("JSON Content:", repr(content))
-
-labels = json.loads(content)-[]
-
-
+# Prediction function
 def predict(audio_path):
-
     feature = extract_features(audio_path)
-
     feature = np.expand_dims(feature, axis=0)
 
     prediction = model.predict(feature, verbose=0)
 
     index = np.argmax(prediction)
-
     confidence = float(np.max(prediction))
 
     emotion = labels[str(index)]
 
     return emotion, confidence, prediction[0]
-
